3fb846d2c293260f708871be55f2d7fe53ddf051
Varya Deviaterikova <varvara_deviaterikova@epam.com>
2021-02-12 22:50:43 +0300

    feat: add hot reload and test query param

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(8000, () => console.log('Server is listening on port 8000'))
+app.use(express.json());
+
+const articlesInfo = {
+  "learn-react": { upvotes: 0, comments: [] },
+  "start-blog": { upvotes: 0, comments: [] },
+  "design-concepts": { upvotes: 0, comments: [] },
+};
+
+app.get("/api/blog/:path", (req, res) => {
+  const { path } = req.params;
+  const info = articlesInfo[path];
+  const { test } = req.query;
+  console.log(`test ${test}`);
+
+  if (info) {
+    res.json(info);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(8000, () => console.log("Server is listening on port 8000!"));
